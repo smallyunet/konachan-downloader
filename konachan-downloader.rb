f3ff612d79d93640c2ec80d10b401452c1ea8534
smallyunet <dev.wangyu@gmail.com>
2025-12-29 12:58:31 +0800

    build: update konachan-downloader to v0.0.10, refresh homepage URL, and update screenshot.

--- konachan-downloader.rb
+++ konachan-downloader.rb
@@ -2,9 +2,9 @@ class KonachanDownloader < Formula
   include Language::Python::Virtualenv
 
   desc "Professional multi-threaded downloader for konachan.net"
-  homepage "https://github.com/yourusername/konachan-downloader"
-  url "https://files.pythonhosted.org/packages/2c/af/5c8f5ce831bb6488ef360bcd12dc55c30024114e32253ecece0232237158/konachan_downloader-0.0.9.tar.gz"
-  sha256 "7134c72184a920eaa5b312c86efa4d85ce65f48b028d06bb4e6b21d6b2f172d3"
+  homepage "https://github.com/smallyunet/konachan-downloader"
+  url "https://files.pythonhosted.org/packages/35/53/b966e6a62336882cf0d3d094e405626d5541610fac2e98003e11777028f0/konachan_downloader-0.0.10.tar.gz"
+  sha256 "6f2f4359280c940a3fb3fa8dee7f234432418f7c206928d7e61cba035e644638"
   license "MIT"
 
   depends_on "python@3.11"
